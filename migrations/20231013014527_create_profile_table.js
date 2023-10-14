e3f03739006b512617c92a8e3c6fc789786ac194
Marwan KN <marwannofal@gmail.com>
2023-10-14 10:40:07 -0400

    Add donation_status and triggers migration files

--- migrations/20231013014527_create_profile_table.js
+++ migrations/20231013014527_create_profile_table.js
@@ -32,4 +32,6 @@ exports.up = function (knex) {
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function (knex) {};
+exports.down = function (knex) {
+  return knex.schema.dropTable("profile");
+};
